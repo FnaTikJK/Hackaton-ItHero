f12b5b2d8530643049c25bd6408870bb1603c41b
Iceneo <nosminin@rit-it.com>
2023-09-14 20:21:51 +0500

    dto added

--- src/app/shared/services/entities/profile.service.ts
+++ src/app/shared/services/entities/profile.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpService} from "../http.service";
 import {ISpecialization} from "./company-entities.service";
+import {ICompany, ICompanyDTO} from "./company.service";
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +12,29 @@ export class ProfileService {
     private httpS: HttpService
   ) { }
 
-  public createProfile$(profileData: IProfileData) {
+  public createProfile$(profileData: IProfileDataDTO) {
     return this.httpS.post('Profiles', profileData);
   }
 }
 
 export interface IProfileData{
+  "id": string;
   "secondName": string;
   "firstName": string;
   "thirdName": string;
-  "specialization": number[];
+  "specialization": ISpecialization[];
+  company: ICompany;
   "phoneNumber": string;
   "email": string;
 }
+
+export interface IProfileDataDTO{
+  "secondName": string;
+  "firstName": string;
+  "thirdName": string;
+  "specialization": string[];
+  companyId: string;
+  "phoneNumber": string;
+  "email": string;
+}
+
